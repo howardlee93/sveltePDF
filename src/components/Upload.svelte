f21e33a41d1d61286eb1c9e5a57cfa0e50a0df58
Howard <howardl93@yahoo.com>
2021-10-05 11:02:38 -0700

    init

---
+++ src/components/Upload.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let files;
+    export let pdfUrl; 
+
+    import View from './View.svelte'; 
+
+</script>
+
+<h1>Upload your file or use a link</h1>
+<input type="file" bind:files>
+<p>
+    <input type="text" bind:value={pdfUrl} >
+</p>
+
+{#if files}
+    <p>You uploaded {files[0].name}</p>
+    {:else if pdfUrl}
+    <p>You inputed {pdfUrl}</p>
+{/if}
+
+<View pdfUrl={pdfUrl} file={files[0]}/> 
+
---
+++ src/components/View.svelte
@@ -0,0 +1,62 @@
+<script>
+    import { onMount } from "svelte";
+    import * as pdfjsLib from "pdfjs-dist";
+    import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";  
+    let canvasRef;
+    export let pdfUrl;
+
+    $:pdfUrl;
+
+    // const data = 'https://www.medicare.gov/Pubs/pdf/10050-medicare-and-you.pdf';
+    // const data = './50-writing-tools.pdf';
+    // const data = "https://nearlaw.com/PDF/MumbaiHC/2018/2018(7)-ALL-MR-95.pdf";
+
+    // The workerSrc property shall be specified.
+    pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
+
+    const load = async (pdfUrl) =>{
+        
+         pdfjsLib
+        .getDocument(pdfUrl).promise
+        .then(doc => doc.getPage(1))
+        .then(page => {
+            const scale = 1.5;
+            const viewport = page.getViewport({ scale });
+
+            // Prepare canvas using PDF page dimensions
+            var context = canvasRef.getContext('2d');
+            canvasRef.height = viewport.height;
+            canvasRef.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+
+            page.render(renderContext);
+        });
+    }
+
+    onMount(async()=>{
+        if(pdfUrl){
+            $:load(pdfUrl);
+        }else if (file.type == 'application/pdf'){
+
+            convert(files)
+            $:load(files);
+        }
+    })
+
+//    $:load(pdfUrl);
+ 
+</script>
+
+
+{#if pdfUrl}
+    <p>View: {pdfUrl}</p>
+    {:else if file}
+    <p>View: {file}</p>
+{/if}
+<canvas bind:this={canvasRef} />
