746f90e8ef9c2f524568a4c8805ca797df3a5f89
Howard <howardl93@yahoo.com>
2021-10-12 22:22:37 -0700

    adding ability to use link

--- src/components/View.svelte
+++ src/components/View.svelte
@@ -2,23 +2,23 @@
     import * as pdfjsLib from "pdfjs-dist";
     import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";  
     let canvasRef;
-    // export let pdfUrl;
+    export let pdfUrl;
     export let typed;
     let initialized = false;
 
     // const data = 'https://www.medicare.gov/Pubs/pdf/10050-medicare-and-you.pdf';
-    // const data = './50-writing-tools.pdf';
     // const data = "https://nearlaw.com/PDF/MumbaiHC/2018/2018(7)-ALL-MR-95.pdf";
 
     // The workerSrc property shall be specified.
     pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
 
     let currentPage = 1; 
-    let numPages = 0;
+    let numPages;
     let pdfRef; 
 
     const renderPages = (page)=>{
-        console.log(currentPage)
+        console.log(pdfRef);
+        numPages = pdfRef.numPages;
         pdfRef && pdfRef.getPage(page)
         .then(page => {
             const scale = 1.5;
@@ -37,18 +37,22 @@
         });
     }
 
-    const load = async (pdfUrl) =>{  
-        pdfRef = await pdfjsLib.getDocument(pdfUrl).promise
+    const load = async (src) =>{  
+        pdfRef = await pdfjsLib.getDocument(src).promise
+        // console.log(pdfRef);
         initialized = true;
     };
 
-    $:if (typed){
-        load(typed)
+    $:if (typed || pdfUrl){
+        if(typed){
+            load(typed);
+        }else if(pdfUrl){
+            load(pdfUrl);
+        }
     };
 
     $: if(initialized) renderPages(currentPage);
 
-   
 </script>
 
 <style>
@@ -58,17 +62,12 @@
     }
 </style>
 
-{#if typed}
+{#if pdfRef}
 <canvas bind:this={canvasRef} />
 {/if}
 
 <div class="center">
-    <button on:click={()=>{ if(currentPage > 1){
-        currentPage -=1;
-        }}
-    }> Previous page</button>
-    <button on:click={()=>{if(currentPage < numPages){
-        currentPage +=1
-        }}
-    }>Next page</button>
+    <button on:click={()=>{ if(currentPage > 1) currentPage --}}> Previous page</button>
+    <button on:click={()=> { if(currentPage < numPages)currentPage ++}}>Next page</button>
 </div>
+{currentPage}
